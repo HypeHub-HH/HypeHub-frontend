aea7ba944b58225c9cff2f13be7474d3fe7c2c3c
Mikołaj Zgórski <119301272+Meek0hWhy@users.noreply.github.com>
2023-10-11 11:25:50 +0200

    Added not found route with blank page and added PrivateRoutes component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import PrivateRoutes from './utils/PrivateRoutes';
 
 function App() {
-  return <>
-  <Routes>
-    <Route path="/" element={<Landing/>}/>
-    <Route path="/tbd" element={<Home/>}/>
-  </Routes>
-  </>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/tbd" element={<Home />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/tbd2"/>
+          <Route path="/tbd3"/>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
