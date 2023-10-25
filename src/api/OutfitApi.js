d1cb536acb6a5316f2cd60e21d5b995ff427b0a6
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-10-25 10:50:24 +0200

    Edited NotFound page

--- src/api/OutfitApi.js
+++ src/api/OutfitApi.js
@@ -4,5 +4,13 @@ import authorizedApi from '../services/AuthorizedAxios.js';
 const baseURL = process.env.REACT_APP_BACKEND_HOST;
 
 export class OutfitApi {
+  static getOutfitAsync = async (id) => await axios.get(`${baseURL}/Outfits/${id}`);
 
+  static getOutfitWithAccountAndLikesAndImagesAndItemsAsync = async (id) =>
+    await axios.get(`${baseURL}/Outfits/${id}/AllInformation`);
+
+  static getLatestOutfitsAsync = async (page, count) =>
+    await axios.get(`${baseURL}/Outfits/Latest?page=${page}&count=${count}`);
+
+  static createOutfitAsync = async (body) => await authorizedApi.post(`/Outfits`, body);
 }
